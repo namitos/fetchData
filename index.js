7bbba824c7f707fe7f98e226ed3a3fbf8cff4def
Artem <namitos@users.noreply.github.com>
2017-10-10 18:14:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,31 @@ const zlib = require('zlib');
 const util = require('util');
 const HttpsProxyAgent = require('https-proxy-agent');
 
-module.exports = (urlFull, headers = {}, body, method = "GET", proxy) => {
+module.exports = (urlFull, headers, body, method, proxy, basicAuth) => {
+  if (urlFull instanceof Object) {
+    let params = urlFull;
+    headers = params.headers;
+    body = params.body;
+    method = params.method;
+    proxy = params.proxy;
+    basicAuth = params.basicAuth;
+    urlFull = params.url;
+  }
+
   let urlParsed = url.parse(urlFull);
   let httpLib = urlParsed.protocol === 'https:' ? https : http;
   return new Promise((resolve, reject) => {
     let parts = Buffer.from('');
-    let agent = proxy ? new HttpsProxyAgent(Object.assign({ secureProxy: true }, proxy)) : null;
+    let agent = proxy ? new HttpsProxyAgent(`http://${proxy.host}:${proxy.port}`) : null;
     let request = {
-      method: method,
+      method: method || 'GET',
       rejectUnauthorized: false,
       host: urlParsed.hostname,
       port: urlParsed.port ? urlParsed.port : urlParsed.protocol === 'https:' ? 443 : 80,
       path: urlParsed.path,
-      headers: headers,
-      agent: agent
+      headers: headers || {},
+      agent: agent,
+      auth: basicAuth
     };
     let req = httpLib.request(request, (res) => {
       res.on('data', (data) => {
